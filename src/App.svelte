18d46b1a2703e2290bb5fab4f610e3b83669f0ad
Anne-Sophie Pereira De Sá <annpereiradesa@linkedin.com>
2025-04-30 17:57:30 +0100

    adding the data by episode

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,59 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+import svelteLogo from "./assets/svelte.svg";
+import LineChart from "./components/LineChart.svelte";
+import * as d3 from "d3";
+
+let rawData;
+let newData;
+
+d3.csv("../public/data/by_episode.csv")
+  .then((data) => {
+    rawData = data;
+
+    newData = rawData.map((d) => {
+      return {
+        seriesRank: +d.seriesRank,
+        seriesTitle: d.seriesTitle,
+        seriesLink: d.seriesLink,
+        episodeNumber: +d.episodeNumber,
+        episodeRating: +d.episodeRating,
+        episodeSeason: +d.episodeSeason,
+        episodeSynopsis: d.episodeSynopsis,
+        episodeVotes: +d.episodeVotes,
+        seriesNumberOfEpisodes: +d.seriesNumberOfEpisodes
+      };
+    });
+  })
+  .catch((error) => {
+    console.error("Error loading the CSV file:", error);
+  });
+
+// $: console.log(rawData);
+$: console.log(newData);
 </script>
 
 <main>
   <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
+  <h1>starting page</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    TEST TEST TEST
-  </p>
+  <p>TEST TEST TEST</p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.svelte:hover {
+  filter: drop-shadow(0 0 2em #ff3e00aa);
+}
 </style>
---
+++ src/components/LineChart.svelte
@@ -0,0 +1,31 @@
+<script>
+  import * as d3 from "d3";
+
+  // // Receive plot data as prop.
+
+  // // The chart dimensions and margins as optional props.
+  // export let width = 928;
+  // export let height = 500;
+  // export let marginTop = 20;
+  // export let marginRight = 30;
+  // export let marginBottom = 30;
+  // export let marginLeft = 40;
+
+  // // Create the x (horizontal position) scale.
+  // const xScale = d3.scaleUtc(
+  //   d3.extent(data, (d) => new Date(d.date)),
+  //   [marginLeft, width - marginRight]
+  // );
+
+  // // Create the y (vertical position) scale.
+  // const yScale = d3.scaleLinear(
+  //   [0, d3.max(data, (d) => d.close)],
+  //   [height - marginBottom, marginTop]
+  // );
+
+  // // Create the line generator.
+  // const line = d3
+  //   .line()
+  //   .x((d) => xScale(new Date(d.date)))
+  //   .y((d) => yScale(d.close));
+</script>
